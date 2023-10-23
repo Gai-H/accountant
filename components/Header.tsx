21c3121b11e98fd1bd031d2851376b02415d6d45
Gai <mail@gaishi.dev>
2023-10-24 01:09:29 +0900

    feat: add create button to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,26 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import { Github } from "lucide-react"
+import { Github, Plus } from "lucide-react"
 
 function Header() {
   return (
     <header className={"sticky top-0 w-full border-b"}>
-      <div className={"flex h-12 items-center justify-between lg:mx-auto lg:w-3/5"}>
+      <div className={"flex h-14 items-center lg:mx-auto lg:w-3/5"}>
         <Link
           href={"/"}
           className={"mr-4 text-lg font-semibold"}
         >
           Accountant
         </Link>
+        <Button
+          className="ml-auto mr-4"
+          asChild
+        >
+          <Link href="/create">
+            <Plus className="mr-2 h-4 w-4" />
+            記録を追加
+          </Link>
+        </Button>
         <Button
           variant={"outline"}
           size={"icon"}

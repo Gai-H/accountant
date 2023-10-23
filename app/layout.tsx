552e6d9a63f67e41fc2fa9b802813e87807244c7
Gai <mail@gaishi.dev>
2023-10-23 20:43:15 +0900

    chore: prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,34 @@
-import type { Metadata } from 'next'
-import { Inter as FontSans } from 'next/font/google'
-import './globals.css'
-import {cn} from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
 import Header from "@/components/Header";
 
 const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: "--font-sans"
-})
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
-  title: 'Accountant',
-}
+  title: "Accountant",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ja">
-    <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-    <Header />
-    {children}
-    </body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,31 @@
-import Link from "next/link";
+import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import { Github } from "lucide-react";
+import { Github } from "lucide-react"
 
 function Header() {
-    return (
-        <header className={"sticky top-0 w-full border-b"}>
-            <div className={"lg:mx-auto lg:w-3/5 h-12 flex items-center justify-between"}>
-                <Link href={"/"} className={"font-semibold text-lg mr-4"}>Accountant</Link>
-                <Link href={"https://github.com/Gai-H/accountant"} target={"_blank"}>
-                    <Button variant={"outline"} size={"icon"}>
-                        <Github className={"h-4 w-4"} />
-                    </Button>
-                </Link>
-            </div>
-        </header>
-    )
+  return (
+    <header className={"sticky top-0 w-full border-b"}>
+      <div className={"flex h-12 items-center justify-between lg:mx-auto lg:w-3/5"}>
+        <Link
+          href={"/"}
+          className={"mr-4 text-lg font-semibold"}
+        >
+          Accountant
+        </Link>
+        <Link
+          href={"https://github.com/Gai-H/accountant"}
+          target={"_blank"}
+        >
+          <Button
+            variant={"outline"}
+            size={"icon"}
+          >
+            <Github className={"h-4 w-4"} />
+          </Button>
+        </Link>
+      </div>
+    </header>
+  )
 }
 
-export default Header;
+export default Header

b4bd4927e9b052aca6ebe6728d61d6f77f5761c1
Gai <mail@gaishi.dev>
2023-10-26 02:03:57 +0900

    feat: add create button to index

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 import Table from "@/components/index/table"
 import PageTitle from "@/components/page-title"
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
+import Link from "next/link"
 
 export default async function Home() {
   return (
     <>
-      <PageTitle>記録一覧</PageTitle>
+      <div className="mb-2 flex justify-between">
+        <PageTitle>記録一覧</PageTitle>
+        <Button
+          className="ml-auto"
+          asChild
+        >
+          <Link href="/create">
+            <Plus className="mr-2 h-4 w-4" />
+            記録を追加
+          </Link>
+        </Button>
+      </div>
       <Table />
     </>
   )

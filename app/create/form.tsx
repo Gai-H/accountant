934fe072fb2334bb30dc1db102abd3e91ebd6657
Gai <mail@gaishi.dev>
2023-10-30 00:28:50 +0900

    feat: recreate form

--- app/create/form.tsx
+++ app/create/form.tsx
@@ -1,27 +1,66 @@
+// TODO: コンポーネント分割する
+
 "use client"
 
-import { useForm, UseFormReturn } from "react-hook-form"
+import { useState } from "react"
+import { useController, useForm, UseFormReturn } from "react-hook-form"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { Plus, Trash2 } from "lucide-react"
 import { FormControl, FormField, FormItem, FormLabel, FormMessage, Form as ShadcnForm } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
 import { Textarea } from "@/components/ui/textarea"
-import { Plus } from "lucide-react"
+import { Checkbox } from "@/components/ui/checkbox"
+import PageTitle from "@/components/page-title"
 import { currencies } from "@/lib/currency"
 import { users } from "@/lib/users"
-import PageTitle from "@/components/page-title"
 
-const formSchema = z.object({
-  title: z.string().min(1, { message: "必須項目です" }).max(100, { message: "最大100文字です" }),
-  description: z.string().max(2000, { message: "最大2000文字です" }).optional(),
-  amount: z.number({ invalid_type_error: "数値を入力してください", required_error: "必須項目です", coerce: true }),
-  currency: z.enum(currencies as any),
-  from: z.array(z.string()).min(1, { message: "必須項目です" }),
-  to: z.array(z.string()).min(1, { message: "必須項目です" }),
-})
+const formSchema = z
+  .object({
+    title: z.string().min(1, { message: "必須項目です" }).max(100, { message: "最大100文字です" }),
+    description: z.string().max(2000, { message: "最大2000文字です" }).optional(),
+    currency: z.enum(currencies as any),
+    from: z
+      .array(
+        z.object({
+          discordId: z.string(),
+          amount: z.number(),
+        }),
+      )
+      .min(1, { message: "必須項目です" })
+      .superRefine((values, ctx) => {
+        for (const value of values) {
+          if (value.discordId === "") ctx.addIssue({ code: z.ZodIssueCode.custom, message: "未選択の人の欄があります" })
+          if (value.amount === Number.MIN_SAFE_INTEGER) ctx.addIssue({ code: z.ZodIssueCode.custom, message: "未入力の金額の欄があります" })
+          if (value.amount === 0) ctx.addIssue({ code: z.ZodIssueCode.custom, message: "金額の欄には0以外を入力してください" })
+        }
+      }),
+    to: z
+      .array(
+        z.object({
+          discordId: z.string(),
+          amount: z.number(),
+        }),
+      )
+      .min(1, { message: "必須項目です" })
+      .superRefine((values, ctx) => {
+        for (const value of values) {
+          if (value.discordId === "") ctx.addIssue({ code: z.ZodIssueCode.custom, message: "未選択の人の欄があります" })
+          if (value.amount === Number.MIN_SAFE_INTEGER) ctx.addIssue({ code: z.ZodIssueCode.custom, message: "未入力の金額の欄があります" })
+          if (value.amount === 0) ctx.addIssue({ code: z.ZodIssueCode.custom, message: "金額の欄には0以外を入力してください" })
+        }
+      }),
+  })
+  .refine(
+    (values) => {
+      const fromAmount = values.from.reduce((prev, current) => prev + current.amount, 0)
+      const toAmount = values.to.reduce((prev, current) => prev + current.amount, 0)
+      return fromAmount === toAmount
+    },
+    { message: "貸し借りの金額が一致しません", path: ["to"] },
+  )
 
 function Form() {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -61,22 +100,13 @@ function Form() {
         </div>
         <div className="md:w-80">
           <div className="flex items-end gap-2">
-            <div className="flex-grow">
-              <AmountFormField {...form} />
-            </div>
             <div className="w-24 shrink-0">
               <CurrencyFormField {...form} />
             </div>
           </div>
         </div>
-        <UserSelectFormField
-          {...form}
-          name="from"
-        />
-        <UserSelectFormField
-          {...form}
-          name="to"
-        />
+        <FromFormField {...form} />
+        <ToFormField {...form} />
         <div className="md:w-[40em]">
           <DescriptionFormField {...form} />
         </div>
@@ -86,6 +116,7 @@ function Form() {
         >
           追加
         </Button>
+        <FormMessage />
       </form>
     </ShadcnForm>
   )
@@ -118,30 +149,6 @@ function TitleFormField({ control }: UseFormReturn<z.infer<typeof formSchema>>) 
   )
 }
 
-function AmountFormField({ control }: UseFormReturn<z.infer<typeof formSchema>>) {
-  return (
-    <FormField
-      control={control}
-      name="amount"
-      render={({ field }) => (
-        <FormItem>
-          <div className="flex items-center gap-2">
-            <FormLabel className={LABEL_CSS}>金額</FormLabel>
-            <FormMessage />
-          </div>
-          <FormControl>
-            <Input
-              {...field}
-              type="number"
-              value={field.value ?? ""}
-            />
-          </FormControl>
-        </FormItem>
-      )}
-    />
-  )
-}
-
 function CurrencyFormField({ control }: UseFormReturn<z.infer<typeof formSchema>>) {
   return (
     <FormField
@@ -149,7 +156,7 @@ function CurrencyFormField({ control }: UseFormReturn<z.infer<typeof formSchema>
       name="currency"
       render={({ field }) => (
         <FormItem>
-          <FormLabel className={LABEL_CSS}></FormLabel>
+          <FormLabel className={LABEL_CSS}>通貨</FormLabel>
           <Select
             onValueChange={field.onChange}
             defaultValue={field.value}
@@ -177,46 +184,206 @@ function CurrencyFormField({ control }: UseFormReturn<z.infer<typeof formSchema>
   )
 }
 
-type UserSelectFormFieldProps = UseFormReturn<z.infer<typeof formSchema>> & {
-  name: "from" | "to"
-}
-
-function UserSelectFormField({ control, name }: UserSelectFormFieldProps) {
+function FromFormField({ control }: UseFormReturn<z.infer<typeof formSchema>>) {
   return (
     <FormField
       control={control}
-      name={name}
-      render={() => (
+      name="from"
+      render={({ field }) => (
         <FormItem>
+          {/* <p>{JSON.stringify(field.value)}</p> */}
           <div className="flex items-center gap-2">
-            <FormLabel className={LABEL_CSS}>{name.charAt(0).toUpperCase() + name.slice(1)}</FormLabel>
+            <FormLabel className={LABEL_CSS}>貸す人</FormLabel>
             <FormMessage />
           </div>
-          {users.map((user) => (
-            <FormField
-              key={user}
-              control={control}
-              name={name}
-              render={({ field }) => {
-                return (
-                  <FormItem
-                    key={user}
-                    className="flex flex-row items-start space-x-3 space-y-0"
-                  >
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value?.includes(user)}
-                        onCheckedChange={(checked) => {
-                          return checked ? field.onChange([...field.value, user]) : field.onChange(field.value?.filter((value) => value !== user))
-                        }}
-                      />
-                    </FormControl>
-                    <FormLabel className="text-inherit">{user}</FormLabel>
-                  </FormItem>
-                )
+          {field.value.map((_, index) => (
+            <div
+              key={`from-div-${index}`}
+              className="flex gap-2"
+            >
+              <Select
+                key={`from-select-${index}`}
+                onValueChange={(value) => {
+                  const newFieldValue = [...field.value]
+                  newFieldValue[index].discordId = value
+                  field.onChange(newFieldValue)
+                }}
+                value={field.value[index].discordId}
+              >
+                <SelectTrigger className="w-24">
+                  <SelectValue placeholder="人" />
+                </SelectTrigger>
+                <SelectContent>
+                  {users.map((user) => (
+                    <SelectItem
+                      value={user}
+                      key={`from-selectitem-${user}`}
+                      disabled={field.value.some((item) => item.discordId === user)}
+                    >
+                      {user}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <Input
+                type="number"
+                value={field.value[index].amount === Number.MIN_SAFE_INTEGER ? "" : field.value[index].amount}
+                placeholder="金額"
+                className="w-32"
+                onChange={(e) => {
+                  const newFieldValue = [...field.value]
+                  newFieldValue[index].amount = e.target.value.length === 0 ? Number.MIN_SAFE_INTEGER : Number(e.target.value)
+                  field.onChange(newFieldValue)
+                }}
+              />
+              <Button
+                size="icon"
+                variant="destructive"
+                type="button"
+                onClick={() => field.onChange(field.value.filter((_, i) => i !== index))}
+              >
+                <Trash2 className="h-4 w-4"></Trash2>
+              </Button>
+            </div>
+          ))}
+          <Button
+            variant="secondary"
+            type="button"
+            disabled={field.value?.length === users.length}
+            onClick={() => field.onChange([...field.value, { discordId: "", amount: Number.MIN_SAFE_INTEGER }])}
+            className="block w-32"
+          >
+            貸す人を追加
+          </Button>
+        </FormItem>
+      )}
+    />
+  )
+}
+
+function ToFormField({ control }: UseFormReturn<z.infer<typeof formSchema>>) {
+  const { field: fromField } = useController({ name: "from" })
+  const [split, setSplit] = useState<boolean>(true)
+
+  const getSplitAmount = (numberOfToPeople: number) => {
+    const fromValues: z.infer<typeof formSchema>["from"] = fromField.value
+    if (fromValues.some((f) => f.amount === Number.MIN_SAFE_INTEGER)) return Number.MIN_SAFE_INTEGER
+    const fromAmountSum = fromValues.reduce((prev, current) => prev + current.amount, 0)
+    if (fromAmountSum === 0) return 0 // ゼロ除算
+    return fromAmountSum / numberOfToPeople
+  }
+
+  return (
+    <FormField
+      control={control}
+      name="to"
+      render={({ field }) => (
+        <FormItem>
+          {/* <p>{JSON.stringify(field.value)}</p> */}
+          <div className="flex items-center gap-2">
+            <FormLabel className={LABEL_CSS}>借りる人</FormLabel>
+            <FormMessage />
+          </div>
+          <div className="flex w-fit items-center gap-2 rounded-md border px-3 py-3">
+            <Checkbox
+              id="to-checkbox-split"
+              checked={split}
+              onCheckedChange={(checked) => {
+                setSplit(checked === "indeterminate" ? false : checked)
+                if (checked === "indeterminate" || checked === false) {
+                  // チェックが消えたとき、割り勘で入力された小数が残ると嫌なので金額をリセット
+                  field.onChange(field.value.map((item) => ({ ...item, amount: Number.MIN_SAFE_INTEGER })))
+                } else {
+                  // チェックがついたときに割り勘する
+                  const splitAmount = getSplitAmount(field.value.length)
+                  field.onChange(field.value.map((item) => ({ ...item, amount: splitAmount })))
+                }
               }}
             />
+            <label
+              htmlFor="to-checkbox-split"
+              className="cursor-pointer text-sm font-medium leading-none"
+            >
+              割り勘する
+            </label>
+          </div>
+          {field.value.map((_, index) => (
+            <div
+              key={`from-div-${index}`}
+              className="flex gap-2"
+            >
+              <Select
+                key={`from-select-${index}`}
+                onValueChange={(value) => {
+                  const newFieldValue = [...field.value]
+                  newFieldValue[index].discordId = value
+                  field.onChange(newFieldValue)
+                }}
+                value={field.value[index].discordId}
+              >
+                <SelectTrigger className="w-24">
+                  <SelectValue placeholder="人" />
+                </SelectTrigger>
+                <SelectContent>
+                  {users.map((user) => (
+                    <SelectItem
+                      value={user}
+                      key={`from-selectitem-${user}`}
+                      disabled={field.value.some((item) => item.discordId === user)}
+                    >
+                      {user}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <Input
+                type="number"
+                value={field.value[index].amount === Number.MIN_SAFE_INTEGER ? "" : field.value[index].amount}
+                placeholder="金額"
+                className="w-32"
+                onChange={(e) => {
+                  if (split) return
+                  const newFieldValue = [...field.value]
+                  newFieldValue[index].amount = e.target.value.length === 0 ? Number.MIN_SAFE_INTEGER : Number(e.target.value)
+                  field.onChange(newFieldValue)
+                }}
+                disabled={split}
+              />
+              <Button
+                size="icon"
+                variant="destructive"
+                type="button"
+                onClick={() => {
+                  const removed = field.value.filter((_, i) => i !== index)
+                  if (split) {
+                    const splitAmount = getSplitAmount(removed.length)
+                    field.onChange(removed.map((item) => ({ ...item, amount: splitAmount })))
+                  } else {
+                    field.onChange(removed)
+                  }
+                }}
+              >
+                <Trash2 className="h-4 w-4"></Trash2>
+              </Button>
+            </div>
           ))}
+          <Button
+            variant="secondary"
+            type="button"
+            disabled={field.value?.length === users.length}
+            onClick={() => {
+              const updated = [...field.value, { discordId: "", amount: Number.MIN_SAFE_INTEGER }]
+              if (split) {
+                const splitAmount = getSplitAmount(updated.length)
+                field.onChange(updated.map((item) => ({ ...item, amount: splitAmount })))
+              } else {
+                field.onChange(updated)
+              }
+            }}
+            className="block w-32"
+          >
+            貸りる人を追加
+          </Button>
         </FormItem>
       )}
     />

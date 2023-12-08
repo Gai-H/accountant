40a8f50f31e482b874e55ddd6b7f47bb4fa03328
Gai <mail@gaishi.dev>
2023-12-08 23:03:35 +0900

    fix: remove unnecessary component

--- components/settings/currency/currencySettingCard.tsx
+++ components/settings/currency/currencySettingCard.tsx
@@ -1,35 +1,14 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Skeleton } from "@/components/ui/skeleton"
 import { Currency } from "@/types/firebase"
 import CurrencySettingForm from "./currencySettingForm"
-import CurrencySettingFormSkeleton from "./currencySettingFormSkeleton"
 
-type CurrencySettingCardProps =
-  | {
-      currency: Currency & {
-        id: string
-      }
-    }
-  | {
-      loading: true
-    }
+type CurrencySettingCardProps = {
+  currency: Currency & {
+    id: string
+  }
+}
 
 function CurrencySettingCard(props: CurrencySettingCardProps) {
-  if ("loading" in props) {
-    return (
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-base">
-            <Skeleton className="h-6 w-12" />
-          </CardTitle>
-          <CardContent className="p-0">
-            <CurrencySettingFormSkeleton />
-          </CardContent>
-        </CardHeader>
-      </Card>
-    )
-  }
-
   return (
     <Card>
       <CardHeader>

65e692b033d585939e21596d083aa8a624a5c566
Gai <mail@gaishi.dev>
2024-02-09 11:02:22 +0900

    style: change the number of skeletons

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -51,7 +51,7 @@ function CurrencySettingSection() {
       <SectionTitle>通貨</SectionTitle>
       <div className="grid grid-cols-1 gap-2 md:grid-cols-3">
         {isLoading
-          ? Array.from({ length: 3 }).map((_, i) => <CurrencySettingCardSkeleton key={i} />)
+          ? Array.from({ length: 2 }).map((_, i) => <CurrencySettingCardSkeleton key={i} />)
           : propCurrencies.map((pc, i) => (
               <CurrencySettingCard
                 currency={pc}

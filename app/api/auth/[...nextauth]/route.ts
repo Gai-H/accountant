3edc1f1526643bef23f83c5e46dc58e0c6bd54cc
Gai <mail@gaishi.dev>
2024-02-11 16:24:23 +0900

    fix: remove check for a guild that users must join

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,7 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.DISCORD_CLIENT_ID as string,
       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
       authorization: {
-        params: { scope: "identify email guilds" },
+        params: { scope: "identify email" },
       },
     }),
   ],
@@ -28,10 +28,6 @@ export const authOptions: NextAuthOptions = {
     async signIn({ account, profile }) {
       if (account == null || account.access_token == null || profile === undefined) return false
 
-      // check if user is in guild
-      if (!(await isJoinGuild(account.access_token))) return false
-
-      // add user to database
       return await insertUser(profile as DiscordProfile)
     },
   },
@@ -41,31 +37,6 @@ export const authOptions: NextAuthOptions = {
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
-
-type Guild = {
-  features: string[]
-  icon: string
-  id: string
-  name: string
-  owner: boolean
-  permissions: number
-  permissions_new: string
-}
-
-const isJoinGuild = async (accessToken: string): Promise<boolean> => {
-  if (!process.env.DISCORD_GUILD_ID) return false
-
-  const res: Response = await fetch("https://discordapp.com/api/users/@me/guilds", {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  })
-  if (res.ok) {
-    const guilds: Guild[] = await res.json()
-    return guilds.some((guild: Guild) => guild.id === (process.env.DISCORD_GUILD_ID as string))
-  }
-  return false
-}
 
 const insertUser = async (profile: DiscordProfile): Promise<boolean> => {
   const ref = db.ref("users")

7e96edbb88102fb79a4c9ab2f4a04ad06bddc6e3
Gaishi Hirota <mail@gaishi.dev>
2024-02-26 18:59:43 +0900

    refactor: revalidate cache when a transaction is deleted

--- app/transaction/[slug]/actions.ts
+++ app/transaction/[slug]/actions.ts
@@ -1,9 +1,16 @@
 "use server"
 
+import { revalidatePath } from "next/cache"
 import { removeTransaction } from "@/lib/firebase/transactions"
 
 const remove = async (transactionId: string): Promise<boolean> => {
-  return await removeTransaction(transactionId)
+  const res = await removeTransaction(transactionId)
+  if (res) {
+    revalidatePath(`/transaction/${transactionId}`)
+    revalidatePath("/")
+  }
+
+  return res
 }
 
 export { remove }

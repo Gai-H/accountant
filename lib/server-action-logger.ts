6b7b83250ad0ec64e612445fb71953d3e4848b2a
Gaishi Hirota <mail@gaishi.dev>
2024-02-28 22:41:54 +0900

    fix: remove newline from logs

--- lib/server-action-logger.ts
+++ lib/server-action-logger.ts
@@ -18,9 +18,12 @@ const logger = <T extends any[], R>(action: ServerAction<T, R>): ServerAction<T,
 
     const actionId = crypto.randomUUID()
 
-    log.info(`Server Action Request ${actionId} by ${session.user.displayName} (${session.user.id})`, ...args)
+    const argString = args.map((arg) => JSON.stringify(arg)).join(", ")
+    log.info(`Server Action Request ${actionId} by ${session.user.displayName} (${session.user.id}) ${argString}`)
+
     const res = await action(...args)
-    log.info(`Server Action Response ${actionId}`, res as { [key: string]: any } | undefined)
+
+    log.info(`Server Action Response ${actionId} ${JSON.stringify(res)}`)
     await log.flush()
 
     return res
